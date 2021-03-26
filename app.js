0040f555359c27dd23ea3d9f948981932885b6a6
Bilal <bilal.rizwaan1909@gmail.com>
2021-03-26 23:24:37 +0500

    sw.js script

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 if('serviceWorker' in navigator)
 {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('sw.js')
+    .then(register=>{
+        console.log("SW Registered");
+        console.log(register);
+    }).catch(error=>{
+        console.log("SW is not Registered",error)
+    })
+}
+else{
 
 }
