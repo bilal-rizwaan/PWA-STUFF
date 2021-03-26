b25f96b7e71d125574bf8f49e0fc3f849dea1b86
Bilal <bilal.rizwaan1909@gmail.com>
2021-03-26 23:54:13 +0500

    Done

--- sw.js
+++ sw.js
@@ -20,3 +20,54 @@ self.addEventListener("fetch", e => {
         })
     )
 })
+
+//second method ovi
+var app='v1.00';
+
+var files=[
+    "./",
+    "index.html",
+    "style.css",
+    "icon/android-icon-192x192-dunplab-manifest-42526.jpg"
+]
+
+        self.addEventListener('install', e =>{
+            // console.log("Install",e);
+            e.waitUntill(
+                caches.open("app").then(cache => {
+                    return cache.addAll(files)
+                    .catch(err=>{
+                        console.error("Erroe adding files to",err);
+                    })
+                    })
+            )
+            console.info('SW Installed');
+            self.skipWaiting();
+        })
+
+        self.addEventListener('activate',e=>{
+        e.waitUntill(
+            caches.keys()
+            .then(cacheName=>{
+                return Promise.all(
+                    cacheName.map(cache=>{
+                        if(cache !== app){
+                            console.info("Erroe Old Cache",cache);
+                            return caches.delete(cache);
+                        }
+                    })
+                )
+            })
+        )
+        return self.clients.claim();
+      })
+      ///Fetch
+      self.addEventListener('fetch',e=>{
+          console.info("SW fetch",e.request.url);
+          e.respondWidth(
+            caches.match(e.request)
+            .then(response=>{
+                return response || fetch(e.request);
+            })
+        )
+      })
